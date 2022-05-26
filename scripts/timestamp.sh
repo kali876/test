7d98ceabab2d4c5037fd554a0b75f3e64c5bcbad
kali876 <kali6@wp.pl>
2022-05-26 14:38:03 +0200

    Update timestamp.sh

--- scripts/timestamp.sh
+++ scripts/timestamp.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 TIMESTAMP=$(TZ=UTC git show --quiet --date='format-local:%s' --format="%cd")
-var=$(cat pubspec.yaml | grep version: | sed -n '1p' | awk '{print $2}')
+var=$(cat ./pubspec.yaml | grep version: | sed -n '1p' | awk '{print $2}')
 new_var="${var%.*}.$TIMESTAMP"
 
-sed -i '' "s/$var/$new_var/" pubspec.yaml
+sed -i '' "s/$var/$new_var/" ./pubspec.yaml
